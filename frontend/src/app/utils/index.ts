7cab4ae627f3f731d1ba6ccd54f2e2d1b3c59115
viegasdomdev <viegasdomdev@gmail.com>
2020-07-06 17:43:15 +0100

    Removes existing frontend

--- frontend/src/app/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const memoryConverter = (number: number): string => {
-  return (number / 10 ** 9).toFixed(2);
-};
--- odin/src/routes/processes.ts
+++ odin/src/routes/processes.ts
@@ -1,10 +1,33 @@
 import express, { Request, Response } from 'express';
-import { queryProcesses, queryProcess, ProcessesData, ProcessData, killProcess } from '../system';
+import { queryProcesses, queryProcess, killProcess } from '../system';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  queryProcesses().then((processes) => res.status(200).send(processes));
+  queryProcesses().then((processes) => {
+    const search = req.query.search;
+
+    if (!search) {
+      res.status(200).send(processes);
+      return;
+    }
+
+    // Asserts that the search comes in only as a string
+    if (typeof search !== 'string') {
+      res.status(200).send([]);
+      return;
+    }
+
+    const searchedProcesses = processes.filter(
+      (p) =>
+        p.name.toLowerCase().includes(search.trim().toLowerCase()) ||
+        p.status.toLowerCase().includes(search.trim().toLowerCase()) ||
+        p.username.toLowerCase().includes(search.trim().toLowerCase()) ||
+        p.pid.toString().includes(search.trim()),
+    );
+
+    res.status(200).send(searchedProcesses);
+  });
 });
 
 router.get('/:pid', (req: Request, res: Response) => {
